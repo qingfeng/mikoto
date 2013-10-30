cc4e23b4e953e8ca0e59300686a987b60cd22312
qingfeng <qingfeng@douban.com>
2013-10-30 13:24:59 +0800

    建立setup.py，把安装依赖都放到了setup.py，并更新了测试安装的方式

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='mikoto',
+      version='0.0.1',
+      keywords=('markdown', 'Douban'),
+      description='better Markdown Render',
+      license='New BSD',
+
+      url='https://github.com/qingfeng/mikoto',
+      author='qingfeng',
+      author_email='qingfeng@douban.com',
+
+      packages=find_packages(),
+      include_package_data=True,
+      platforms='any',
+      install_requires=['Pygments',
+                        'chardet',
+                        'pytest',
+                        'misaka',
+                        ],
+      dependency_links=[
+            'https://github.com/qingfeng/misaka/tarball/master#egg=misaka'
+        ]
+      )
