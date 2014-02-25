4e2fd0de6c362265c6edf48a62b141c98c7b1692
qingfeng <qingfeng@douban.com>
2014-02-25 09:34:12 +0800

    Upgrade to 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='mikoto',
-      version='0.0.1',
+      version='0.0.2',
       keywords=('markdown', 'Douban'),
       description='better Markdown Render',
       license='New BSD',
